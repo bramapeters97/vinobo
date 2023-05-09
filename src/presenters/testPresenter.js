05a7ef9d1497bede36a54edf83aeeb924a9a141a
bramapeters <bramapeters@hotmail.com>
2023-05-09 14:29:30 +0200

    vinobo
    vinobo

---
+++ src/presenters/testPresenter.js
@@ -0,0 +1,7 @@
+import TestView from "../views/testView";
+
+function TestPresenter(props) {
+  return <TestView />;
+}
+
+export default TestPresenter;
---
+++ src/views/testView.js
@@ -0,0 +1,5 @@
+function TestView(props) {
+  return <p>This is TestView</p>;
+}
+
+export default TestView;
